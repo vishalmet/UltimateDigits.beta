7e78066da6a479f893612d6c942f72f9ad554d7d
vishalmet <vishalmet003@gmail.com>
2024-08-21 22:33:40 +0530

    otp routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/landing-page/LandingPage'
 import SelectionPage from './pages/selection-page/SelectionPage'
 import RealNumber from './pages/real-number/RealNumber'
+import OtpPage from './pages/real-number/OtpPage'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           <Route path='/' element={<LandingPage/>}></Route>
           <Route path='/selection-page' element={<SelectionPage />}></Route>
           <Route path='/real-number' element={<RealNumber/>}></Route>
+          <Route path='/otp-page' element={<OtpPage/>}></Route>
         </Routes>
       </Router>
     </div>
