a3eb25a3472dc209856e42ad3c796d4bdaa78196
vishalmet <vishalmet003@gmail.com>
2024-08-27 21:08:33 +0530

    redux-store

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,23 @@
+// src/redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { combineReducers } from 'redux';
+import { numberSlice } from './numberSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  number: numberSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
