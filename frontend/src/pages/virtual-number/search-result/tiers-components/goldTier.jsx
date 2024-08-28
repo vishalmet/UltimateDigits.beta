4a9341f8204a6b1a26d916412f8f730939a9d0a9
vishalmet <vishalmet003@gmail.com>
2024-08-28 13:45:32 +0530

    ui comp for gold

---
+++ frontend/src/pages/virtual-number/search-result/tiers-components/goldTier.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GoldTier = () => {
+  return (
+    <p className="bg-[#FFD700]/50 w-fit h-fit p-1 px-2 text-xs md:text-sm border border-[#FFD700]/20 text-[#FFD700] rounded-full font-bold">
+      Gold Tier
+    </p>
+  );
+};
+
+export default GoldTier;
