7d96e93128538604a9e787aa8f36dea9641687b8
vishalmet <vishalmet003@gmail.com>
2024-08-28 13:45:15 +0530

    ui comp for silver

---
+++ frontend/src/pages/virtual-number/search-result/tiers-components/silverTier.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SilverTier = () => {
+  return (
+    <p className="bg-[#C0C0C0]/50 w-fit h-fit p-1 px-2 text-xs md:text-sm border border-[#C0C0C0]/20 text-[#C0C0C0] rounded-full font-bold">
+      Silver Tier
+    </p>
+  );
+};
+
+export default SilverTier;
