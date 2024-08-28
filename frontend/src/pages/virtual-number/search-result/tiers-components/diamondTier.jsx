71f6ebb37a86d62820b9862e16f50d11c7d365bc
vishalmet <vishalmet003@gmail.com>
2024-08-28 13:45:00 +0530

    ui comp for diamond

---
+++ frontend/src/pages/virtual-number/search-result/tiers-components/diamondTier.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DiamondTier = () => {
+  return (
+    <p className="bg-[#6366E9]/50 w-fit h-fit p-1 px-2 text-xs md:text-sm border border-[#6366E9]/20 text-[#8F91EF] rounded-full font-bold">
+      Diamond Tier
+    </p>
+  );
+};
+
+export default DiamondTier;
