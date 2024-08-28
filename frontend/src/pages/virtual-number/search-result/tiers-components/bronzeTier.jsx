70305bced62f33a4326a32c8b055a2ac3d598a63
vishalmet <vishalmet003@gmail.com>
2024-08-28 13:44:42 +0530

    ui comp for bronze

---
+++ frontend/src/pages/virtual-number/search-result/tiers-components/bronzeTier.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BronzeTier = () => {
+  return (
+    <p className="bg-[#CD7F32]/50 w-fit h-fit p-1 px-2 text-xs md:text-sm border border-[#CD7F32]/20 text-[#CD7F32] rounded-full font-bold">
+      Bronze Tier
+    </p>
+  );
+};
+
+export default BronzeTier;
