8ffc2d8ba0c0ed52688414f802dae398669a30c6
vishalmet <vishalmet003@gmail.com>
2024-08-29 14:44:14 +0530

    text size adjustment

--- frontend/src/pages/virtual-number/search-result/tiers-components/bronzeTier.jsx
+++ frontend/src/pages/virtual-number/search-result/tiers-components/bronzeTier.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BronzeTier = () => {
   return (
-    <p className="bg-[#CD7F32]/50 w-fit h-fit p-1 px-2 text-xs md:text-sm border border-[#CD7F32]/20 text-[#CD7F32] rounded-full font-bold">
+    <p className="bg-[#CD7F32]/50 w-fit h-fit p-1 px-2 text-xs border border-[#CD7F32]/20 text-[#CD7F32] rounded-full font-bold">
       Bronze Tier
     </p>
   );
