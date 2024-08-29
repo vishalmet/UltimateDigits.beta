23789c9f921854508a950c6a4dd469dce6be7806
vishalmet <vishalmet003@gmail.com>
2024-08-29 14:43:26 +0530

    text size adjustment

--- frontend/src/pages/virtual-number/search-result/tiers-components/silverTier.jsx
+++ frontend/src/pages/virtual-number/search-result/tiers-components/silverTier.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SilverTier = () => {
   return (
-    <p className="bg-[#C0C0C0]/50 w-fit h-fit p-1 px-2 text-xs md:text-sm border border-[#C0C0C0]/20 text-[#C0C0C0] rounded-full font-bold">
+    <p className="bg-[#C0C0C0]/50 w-fit h-fit p-1 px-2 text-xs border border-[#C0C0C0]/20 text-[#C0C0C0] rounded-full font-bold">
       Silver Tier
     </p>
   );
