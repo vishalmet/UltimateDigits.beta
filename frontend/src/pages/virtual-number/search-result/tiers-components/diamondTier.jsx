8a865d9ddcc6615667650d70aa18d8d05132b63b
vishalmet <vishalmet003@gmail.com>
2024-08-29 14:43:59 +0530

    text size adjustment

--- frontend/src/pages/virtual-number/search-result/tiers-components/diamondTier.jsx
+++ frontend/src/pages/virtual-number/search-result/tiers-components/diamondTier.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const DiamondTier = () => {
   return (
-    <p className="bg-[#6366E9]/50 w-fit h-fit p-1 px-2 text-xs md:text-sm border border-[#6366E9]/20 text-[#8F91EF] rounded-full font-bold">
+    <p className="bg-[#6366E9]/50 w-fit h-fit p-1 px-2 text-xs border border-[#6366E9]/20 text-[#8F91EF] rounded-full font-bold">
       Diamond Tier
     </p>
   );
