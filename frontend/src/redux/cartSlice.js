0c9e8670bdf8148d7861fc9df12fa1b6a5e3e658
vishalmet <vishalmet003@gmail.com>
2024-08-30 19:58:48 +0530

    used cartslicing for cards data

---
+++ frontend/src/redux/cartSlice.js
@@ -0,0 +1,26 @@
+// src/redux/cartSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItemToCart: (state, action) => {
+      state.items.push(action.payload);
+    },
+    removeItemFromCart: (state, action) => {
+      state.items = state.items.filter(item => item.id !== action.payload);
+    },
+    clearCart: (state) => {
+      state.items = [];
+    },
+  },
+});
+
+export const { addItemToCart, removeItemFromCart, clearCart } = cartSlice.actions;
+export const selectCartItems = (state) => state.cart.items;
+export default cartSlice.reducer;
